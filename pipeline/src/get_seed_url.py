d06f67f6c9c82fa3881524431aeacd5ea185de2b
Gabriel de Jesus <96556577+gabriel-de-jesus@users.noreply.github.com>
2024-03-02 09:14:37 +0000

    final commit

--- pipeline/src/get_seed_url.py
+++ pipeline/src/get_seed_url.py
@@ -4,7 +4,7 @@ from typing import List
 from googlesearch import search
 from common_utils.utils import Utils, extract_domain
 
-# !/pipeline/src/
+# !/usr/bin/env pipenv
 #
 # get_seed_url.py
 # Gabriel de Jesus (mestregabrieldejesus@gmail.com)
